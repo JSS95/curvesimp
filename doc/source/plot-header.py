ec06424524e8d76fab29030e23afaf2f60aeabbe
Jisoo Song <jeesoo9595@snu.ac.kr>
2024-08-29 00:39:38 +0900

    doc: modify header image

--- doc/source/plot-header.py
+++ doc/source/plot-header.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from curvesimp.agarwal import min_err
+from curvesimp.imaiiri import min_err
 
-x = np.linspace(0, 5, 100)
+x = np.linspace(0, 5, 50)
 f = np.exp(-x) * np.cos(2 * np.pi * x)
 curve = np.column_stack([x, f])
 
-plt.plot(*curve.T, label="original (n=100)")
-for n in [50, 25]:
+plt.plot(*curve.T, label=f"original (n={len(x)})")
+for n in [25, 10]:
     vert, err = min_err(curve, n)
     plt.plot(*vert.T, "--", label=f"n={n} (err={err:.2f})")
 plt.legend()
